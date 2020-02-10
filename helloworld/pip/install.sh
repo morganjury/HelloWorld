d6f7cccd94690e06900d6744c548a0067c958869
morgan <morgan_jury@msn.com>
2020-02-10 14:57:12 +0000

    Updated install.sh to work with latest versions and streamline output.

--- helloworld/pip/install.sh
+++ helloworld/pip/install.sh
@@ -1,8 +1,27 @@
-. /usr/local/bin/virtualenvwrapper.sh
-workon HelloWorld
-if [ $? -eq 0 ]; # if exit status of workon is not 0
+# exec > /dev/null 2>&1 # blocks stdout and stderr
+exec 7>&1 # save a copy of current stdout
+exec >/dev/null # redirect everyone else's stdout to /dev/null
+virtualenv --version
+if [ $? -ne 0 ];
 then
+  printf "Installing VirtualEnv..." >&7
+  pip3 install virtualenv
+fi
+virtualenvwrapper_var=$(which virtualenvwrapper.sh)
+. $virtualenvwrapper_var
+virtualenvwrapper --version
+if [ $? -ne 0 ];
+then
+  printf "Installing VirtualEnvWrapper..." >&7
+  pip3 install virtualenvwrapper
+fi
+workon HelloWorld
+if [ $? -ne 0 ];
+then
+  printf "Creating VirtualEnv..." >&7
   mkvirtualenv HelloWorld
-  setprojectdir .
+  # setprojectdir . # old command
+  setvirtualenvproject .
   pip3 install flask
 fi
+printf "\nProject setup complete.\n\n" >&7
